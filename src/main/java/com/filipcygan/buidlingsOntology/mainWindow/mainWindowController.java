d469d5b1e8993d9630c4203bb0a5c6d447bb3f27
FCygan <filipcygan@outlook.com>
2016-04-19 19:57:30 +0200

    Searching and displaying results in TreeView in mainWindow

--- src/main/java/com/filipcygan/buidlingsOntology/mainWindow/mainWindowController.java
+++ src/main/java/com/filipcygan/buidlingsOntology/mainWindow/mainWindowController.java
@@ -1,37 +1,41 @@
 package com.filipcygan.buidlingsOntology.mainWindow;
 
 import com.filipcygan.buidlingsOntology.model.Building;
-import com.filipcygan.buidlingsOntology.model.SessionFactory;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import com.filipcygan.buidlingsOntology.model.Type;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class mainWindowController {
     @FXML
-    public TableView table;
-    public TableColumn buildingNameCol;
-    public TableColumn typeNameCol;
+    public TreeView buildingsTree;
     @FXML
     private TextField searchField;
 
-    public void search(ActionEvent actionEvent) {
-        final Session session = SessionFactory.getSession();
-        ObservableList<Building> buildings = FXCollections.observableList(session.createCriteria(Building.class)
-                                                                                 .add(Restrictions.like("buildingName",
-                                                                                                        "%" +
-                                                                                                        searchField.getText() +
-                                                                                                        "%"))
-                                                                                 .list());
-        buildingNameCol.setCellValueFactory(new PropertyValueFactory<Building, String>("buildingName"));
-        typeNameCol.setCellValueFactory((new PropertyValueFactory<Building, String>("typeName")));
-        table.setItems(buildings);
-        System.out.println(buildings.toString());
+
+    public void search(Event event) {
+        List<Building> buildings = new ArrayList();
+        TreeItem<String> root = new TreeItem<>();
+        try (Session session = com.filipcygan.buidlingsOntology.model.SessionFactory.getSession()) {
+            buildings = session.createCriteria(Building.class).add(Restrictions.like("buildingName", searchField
+                    .getText() + "%")).list();
+            System.out.println(buildings.toString());
+        }
+
+        for (Building building : buildings) {
+            TreeItem<String> buildingRoot = new TreeItem<>(building.getBuildingName());
+            for (Type type : building.getTypeList()) {
+                buildingRoot.getChildren().add(new TreeItem<String>(type.getTypeName()));
+            }
+            root.getChildren().add(buildingRoot);
+            buildingsTree.setRoot(root);
+        }
     }
 }
