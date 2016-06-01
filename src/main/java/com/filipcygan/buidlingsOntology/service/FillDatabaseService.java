ab1450f182cb7e34712622bdf1345e372d4c0554
FCygan <filipcygan@outlook.com>
2016-06-01 15:13:58 +0200

    Changed dbpedia query

--- src/main/java/com/filipcygan/buidlingsOntology/service/FillDatabaseService.java
+++ src/main/java/com/filipcygan/buidlingsOntology/service/FillDatabaseService.java
@@ -23,19 +23,16 @@ public class FillDatabaseService extends ScheduledService {
             protected Integer call() throws Exception {
                 final Session session = SessionFactory.getSession();
                 for (int offset = 0; ; offset += 1000) {
-                    String JSONAsString = getStringFromURL("http://dbpedia" +
-                            ".org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia" +
-                            ".org&query=SELECT+%3Fname+%3Fsubject+%7B%7B%0D%0ASELECT+%3Fn" +
-                            "ame+%3Fsubject%0D%0A%7B%0D%0A%3Fbuilding+rdfs%3Alabel+%3Fname++.%0D" +
-                            "%0A++%3Fbuilding+a+dbo%3AArchitecturalStructure+.%0D%0A%3Fbuilding+d" +
-                            "ct%3Asubject+%3Fsubject%0D%0Afilter+langMatches%28+lang%28%3Fname%2" +
-                            "9%2C+%22pl%22+%29%0D%0A%7D%0D%0Aorder+by+%3Fbuilding%0D%0A%7D%7D%0D%0Aoffset+" +
-                            offset +
-                            "%0D%0ALIMIT+1000&format=application%2Fsparql-results%2Bjson&CXML_redi" +
-                            "r_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on");
+                    String JSONAsString = getStringFromURL("http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=SELECT+%3Fname+%3Fsubject+%7B%7B%0D%0ASELECT+%3Fname+%3Fsubject%0D%0A%7B%0D%0A%3Fbuilding+rdfs%3Alabel+%3Fname++.%0D%0A++%3Fbuilding+a+dbo%3AArchitecturalStructure+.%0D%0A%3Fbuilding+dbp%3AbuildingType+%3Fsubject%0D%0Afilter+langMatches%28+lang%28%3Fname%29%2C+%22pl%22+%29%0D%0A%7D%0D%0Aorder+by+%3Fbuilding%0D%0A%7D%7D%0D%0Aoffset+" + offset + "" +
+                            "%0D%0ALIMIT+1000&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on");
                     JSONObject jsonObject = new JSONObject(JSONAsString);
                     JSONArray bindings = jsonObject.getJSONObject("results").getJSONArray("bindings");
-                    if (bindings.isNull(0)) break;
+                    System.out.println(bindings.length());
+                    if (bindings.isNull(0)) {
+                        System.out.println(bindings.length());
+                        System.out.println("koniec");
+                        break;
+                    }
 
                     for (int i = 0; i < bindings.length(); i++) {
                         Building building = new Building();
@@ -43,19 +40,30 @@ public class FillDatabaseService extends ScheduledService {
                         String name = bindings.getJSONObject(i).getJSONObject("name").getString("value");
                         Building aaa = (Building) session.createCriteria(Building.class).add(Restrictions.eq
                                 ("buildingName", name)).uniqueResult();
-                        if (aaa != null) building = aaa;
-                        else building.setBuildingName(name);
-                        String typeName = bindings.getJSONObject(i).getJSONObject("subject").getString("value")
-                                .substring(37).
-                                replace('_', ' ');
+                        if (aaa != null) {
+                            building = aaa;
+                        } else {
+                            building.setBuildingName(name);
+                        }
+                        String typeName = bindings.getJSONObject(i).getJSONObject("subject").getString("value");
+                        if (typeName.contains("http://")) {
+                            typeName = typeName.substring(28);
+                            typeName = typeName.replace('_', ' ');
+                        }
+
                         Type type = (Type) session.createCriteria(Type.class).add(Restrictions.eq("typeName",
                                 typeName)).uniqueResult();
-                        if (type != null) building.addType(type);
-                        else building.addType(new Type(typeName));
+                        if (type != null) {
+                            building.addType(type);
+                        } else {
+                            building.addType(new Type(typeName));
+                        }
                         session.saveOrUpdate(building);
                         session.beginTransaction().commit();
                     }
+                    System.out.println(offset);
                 }
+
                 return null;
             }
 
