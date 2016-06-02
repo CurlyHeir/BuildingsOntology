33ba6e6b29e60585fc7b733a6b86d5165bc5140b
FCygan <filipcygan@outlook.com>
2016-06-02 22:15:21 +0200

    Change dbpedia query Add types translation

--- src/main/java/com/filipcygan/buidlingsOntology/Application.java
+++ src/main/java/com/filipcygan/buidlingsOntology/Application.java
@@ -7,18 +7,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Application extends javafx.application.Application {
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/mainWindow.fxml"));
         Scene scene = new Scene(root, 800, 600);
         Service fillDatabaseService = new FillDatabaseService();
         fillDatabaseService.start();
-        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setTitle("Ontologia budynków");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- src/main/java/com/filipcygan/buidlingsOntology/service/FillDatabaseService.java
+++ src/main/java/com/filipcygan/buidlingsOntology/service/FillDatabaseService.java
@@ -1,5 +1,6 @@
 package com.filipcygan.buidlingsOntology.service;
 
+import com.filipcygan.buidlingsOntology.BuildingTypes;
 import com.filipcygan.buidlingsOntology.model.Building;
 import com.filipcygan.buidlingsOntology.model.SessionFactory;
 import com.filipcygan.buidlingsOntology.model.Type;
@@ -11,6 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
@@ -23,34 +25,33 @@ public class FillDatabaseService extends ScheduledService {
             protected Integer call() throws Exception {
                 final Session session = SessionFactory.getSession();
                 for (int offset = 0; ; offset += 1000) {
-                    String JSONAsString = getStringFromURL("http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=SELECT+%3Fname+%3Fsubject+%7B%7B%0D%0ASELECT+%3Fname+%3Fsubject%0D%0A%7B%0D%0A%3Fbuilding+rdfs%3Alabel+%3Fname++.%0D%0A++%3Fbuilding+a+dbo%3AArchitecturalStructure+.%0D%0A%3Fbuilding+dbp%3AbuildingType+%3Fsubject%0D%0Afilter+langMatches%28+lang%28%3Fname%29%2C+%22pl%22+%29%0D%0A%7D%0D%0Aorder+by+%3Fbuilding%0D%0A%7D%7D%0D%0Aoffset+" + offset + "" +
-                            "%0D%0ALIMIT+1000&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on");
+                    String JSONAsString = getStringFromURL("http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+owl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0APREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0APREFIX+dc%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%3E%0D%0APREFIX+%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fresource%2F%3E%0D%0APREFIX+dbpedia2%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fproperty%2F%3E%0D%0APREFIX+dbpedia%3A+%3Chttp%3A%2F%2Fdbpedia.org%2F%3E%0D%0APREFIX+skos%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23%3E%0D%0APREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0A%0D%0ASELECT+%3Fname+%3Fsubject+%7B%7B%0D%0ASELECT+%3Fname+%3Fsubject%0D%0A%7B%0D%0A%3Fbuilding+rdfs%3Alabel+%3Fname++.%0D%0A++%3Fbuilding+a+dbo%3AArchitecturalStructure+.%0D%0A%3Fbuilding+rdf%3Atype+%3Fsubject%0D%0Afilter+langMatches%28+lang%28%3Fname%29%2C+%22pl%22+%29%0D%0A%7D%0D%0Aorder+by+%3Fbuilding%0D%0A%7D%7D%0D%0Aoffset+" + offset + "%0D%0ALIMIT+1000%0D%0A&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on");
                     JSONObject jsonObject = new JSONObject(JSONAsString);
                     JSONArray bindings = jsonObject.getJSONObject("results").getJSONArray("bindings");
-                    System.out.println(bindings.length());
                     if (bindings.isNull(0)) {
-                        System.out.println(bindings.length());
                         System.out.println("koniec");
                         break;
                     }
-
                     for (int i = 0; i < bindings.length(); i++) {
                         Building building = new Building();
-
                         String name = bindings.getJSONObject(i).getJSONObject("name").getString("value");
-                        Building aaa = (Building) session.createCriteria(Building.class).add(Restrictions.eq
+                        Building buildingFromDatabase = (Building) session.createCriteria(Building.class).add(Restrictions.eq
                                 ("buildingName", name)).uniqueResult();
-                        if (aaa != null) {
-                            building = aaa;
+                        if (buildingFromDatabase != null) {
+                            building = buildingFromDatabase;
                         } else {
                             building.setBuildingName(name);
                         }
                         String typeName = bindings.getJSONObject(i).getJSONObject("subject").getString("value");
-                        if (typeName.contains("http://")) {
-                            typeName = typeName.substring(28);
-                            typeName = typeName.replace('_', ' ');
+                        for (BuildingTypes buildingType : BuildingTypes.values()) {
+                            if (typeName.contains(buildingType.name())) {
+                                typeName = buildingType.getName();
+                                break;
+                            }
                         }
-
+                       if (typeName.contains("http")) {
+                            continue;
+                       }
                         Type type = (Type) session.createCriteria(Type.class).add(Restrictions.eq("typeName",
                                 typeName)).uniqueResult();
                         if (type != null) {
@@ -63,11 +64,10 @@ public class FillDatabaseService extends ScheduledService {
                     }
                     System.out.println(offset);
                 }
-
                 return null;
             }
 
-            private String getStringFromURL(String urlString) throws Exception {
+            private String getStringFromURL(String urlString) throws IOException {
 
                 BufferedReader reader = null;
                 try {
